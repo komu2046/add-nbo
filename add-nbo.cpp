08860cb99db30c6d25daea0a463c79f033e82074
komu2046 <komu2046@naver.com>
2023-07-19 21:56:08 +0900

    Add add-nbo modules

---
+++ add-nbo.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include <cstdint>
+#include "add-nbo.h"
+
+void sum(uint8_t input1[], uint8_t input2[]) 
+{
+    uint32_t* p1 = reinterpret_cast<uint32_t*>(input1);
+    uint32_t n1 = ntohl(*p1);
+
+    uint32_t* p2 = reinterpret_cast<uint32_t*>(input2);
+    uint32_t n2 = ntohl(*p2);
+
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n",n1, n1, n2, n2, n1+n2, n1+n2);
+}
